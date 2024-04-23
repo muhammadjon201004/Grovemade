f0c8921d60f6b19de2cb240594d2f72b19eb738d
muhammadjon <gadoyboevmuhammadjon@gmail.com>
2024-04-23 20:03:05 +0500

    readme

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      screens: {
+        sm: '480px',
+        mdd: '761px',
+        md: '768px',
+        MaxMd: '768px',
+        MinLg: '820px',
+        lg: '1154px',
+        xl: '1440px', 
+
+        SmMini:{'max' : '320px'},
+        sm3: {'max' : '480px'},
+        sm2: {'max' : '520px'},
+        sm4: {'max' : '589px'},
+        Bsm2: {'max' : '600px'},
+        Bsm3: {'max' : '679px'},
+        mdd2: {'max' : '761px'},
+        md2: {'max' : '768px'},
+        md3: {'max' : '770px'},
+        MinLg2: {'max' : '820px'}
+
+      }
+    },
+  },
+  plugins: [],
+}
+
